b7d369824fc4c6431986e80ee4fbb8075c4ec7b7
BaraboSH <znikita99@mail.ru>
2018-03-19 19:33:49 +0200

    algo_8

---
+++ Алгоритмы и структуры данных/Algo_8/Algo_8/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo_8
+{
+    class Program
+    {
+        static void Swap(int[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                int temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        // Метод сортировки вставками 
+
+        static public void SelectionSort(int[] items)
+        {
+            int sortedRangeEnd = 0;
+
+            while (sortedRangeEnd < items.Length)
+            {
+                int nextIndex = FindIndexOfSmallestFromIndex(items, sortedRangeEnd);
+                Swap(items, sortedRangeEnd, nextIndex);
+                sortedRangeEnd++;
+            }
+        }
+
+        // Метод находит минимальный элемент в массиве.
+
+        static private int FindIndexOfSmallestFromIndex(int[] items, int sortedRangeEnd)
+        {
+            int currentSmallest = items[sortedRangeEnd];
+            int currentSmallestIndex = sortedRangeEnd;
+
+            for (int i = sortedRangeEnd + 1; i < items.Length; i++)
+            {
+                if (currentSmallest.CompareTo(items[i]) > 0)
+                {
+                    currentSmallest = items[i];
+                    currentSmallestIndex = i;
+                }
+            }
+
+            return currentSmallestIndex;
+        }
+        static private void Print(int[] itemArray)
+        {
+            for (int i = 0; i < itemArray.Length; i++)
+            {
+                Console.Write("{0} ", itemArray[i]);
+                if (i % 20 == 0 && i > 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int[] array = new int[100];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1000);
+            }
+            Console.WriteLine("                   Початковий масив:");
+            Print(array);
+            SelectionSort(array);
+            Console.WriteLine();
+            Console.WriteLine("                   Відсортований масив:");
+            Print(array);
+            Console.WriteLine();
+
+        }
+    }
+}
